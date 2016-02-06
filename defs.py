5858d9c90ed3fef1bd0fda946e9fb6f78b3dc2fa
Adi David <ady.david@gmail.com>
2016-02-06 18:32:47 +0900

    Updates to card implementation.

--- defs.py
+++ defs.py
@@ -25,19 +25,6 @@ class Card(object):
         pass
 
 
-class Staff(Card):
-    def __init__(self, name, cost, description, abilities):
-        super(Staff, self).__init__(name, cost, description)
-        self.original_abilities = abilities
-        self.abilities = abilities
-        self.played = False
-
-    def play(self, player):
-        self.played = True
-        player.unit.append(self)
-        # ...
-
-
 class Reaction(Card):
     def __init__(self, name, cost, description, trigger, effect):
         super(Reaction, self).__init__(name, cost, description)
--- staff.py
+++ staff.py
@@ -6,21 +6,20 @@ whatever adi tried to do with the staff cards
 from defs import *
 
 
-def __init__(self, name, cost, description, abilities):
-    super(Staff, self).__init__(name, cost, description)
-    self.original_abilities = abilities
-    self.abilities = abilities
-    self.played = False
+class Staff(Card):
+    def __init__(self, name, cost, description, abilities):
+        super(Staff, self).__init__(name, cost, description)
+        self.original_abilities = abilities
+        self.abilities = abilities
+        self.played = False
 
+    def play(self, player):
+        self.played = True
+        player.removefromhand(self)
+        player.unit.add_card(self)
 
-def play(self, player):
-    self.played = True
-    player.removefromhand(self)
-    player.unit.add_card(self)
-
-
-def buffs(self, buff):
-    self.abilities += buff
+    def buffs(self, buff):
+        self.abilities += buff
 
 
 
