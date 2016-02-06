591ed5adef1d8995c2b566f8afaba2b28373ce84
James Schloss <james.schloss@oist.jp>
2016-02-06 19:59:02 +0900

    adding gui skeleton to work with.

--- gui.py
+++ gui.py
@@ -8,6 +8,7 @@
 #------------------------------------------------------------------------------#
 
 import curses
+import _curses
 
 # Set up standard screen
 stdscreen = curses.initscr()
@@ -25,8 +26,31 @@ begin_x = 20; begin_y = 7
 height = 5; width = 40
 win = curses.newwin(height, width, begin_y, begin_x)
 
-stdscreen.addstr(1,12, "Help me!")
+# Defining corners
+stdscreen.addch(0,0, curses.ACS_ULCORNER)
+stdscreen.addch(23,0, curses.ACS_LLCORNER)
+stdscreen.addch(0,78, curses.ACS_URCORNER)
+stdscreen.addch(23,78, curses.ACS_LRCORNER)
 
+# Defining borders
+hline = 11
+vline = 59
+for i in range(1,79):
+    stdscreen.addch(0,i, curses.ACS_HLINE)
+    stdscreen.addch(23, i, curses.ACS_HLINE)
+    if i < vline:
+        stdscreen.addch(hline, i, curses.ACS_HLINE)
+    if i <= 22:
+        stdscreen.addch(i, 0, curses.ACS_VLINE)
+        stdscreen.addch(i, 78, curses.ACS_VLINE)
+        stdscreen.addch(i, vline, curses.ACS_VLINE)
+    if i == vline:
+        stdscreen.addch(0, vline, curses.ACS_TTEE)
+        stdscreen.addch(23, vline, curses.ACS_BTEE)
+    if i == hline:
+        stdscreen.addch(i, 0, curses.ACS_LTEE)
+        stdscreen.addch(i, vline, curses.ACS_RTEE)
+        
 stdscreen.getch()
 
 # Terminating curses:
