c91ebf3a81502acafe65538a9b9d46569b6679ac
HtonS <reshodko@gmail.com>
2016-02-07 13:19:12 +0900

    Completed SmallGrant action card class

--- actions.py
+++ actions.py
@@ -140,3 +140,56 @@ class MediumGrant(Action):
         curAg = random.randint(0, len(agencies)-1)
         return "Alas! The %s funding agency decided you are lame and gave the money to someone else."\
         %(agencies[curAg])
+
+class SmallGrant(Action):
+    """ Apply for a small grant. Costs some time and results in a humble budget increase
+    """
+    def __init__(self):
+        name = 'Apply for a Small Grant'
+        cost = defs.Points(0, 3, 0, 0, 0, 0)
+        effect = defs.Points(1, 0, 0, 0, 0, 0)
+        self.bonusIF = 1
+        description = "Time to beg for some pocket cash! Apply for a small grant"\
+                    "by asking for donations at the local science fair.\n"\
+        "Requires a bit of time, but your lab gets some change from the grandma.\n"\
+        "Cost: %s.\nYou gain: %s and %d additional impact factor"\
+                      %(cost.__repr__(), effect.__repr__(), self.bonusIF)
+        super(SmallGrant, self).__init__(name, cost, description, effect)
+
+    def isPlayable(self, player):
+        """
+        :param player: The player who is attempting to play the card
+        :return: True if the card is ok to play and False otherwise
+        """
+        return True if player.points>=self.cost else False
+
+    def play(self, player):
+        """
+        :param player: Player who is playing the card
+        :return: True if success and False if failure
+        """
+        # TODO: Add trigger
+        if self.isPlayable(player):
+            player.points = player.points - self.cost
+            player.points = player.points + self.effect
+            player.impact += self.bonusIF
+            return True
+        else:
+            return False
+
+    @property
+    def successMessage(self):
+        """
+        :return: Message which can be displayed in case of success
+        """
+        curAg = random.randint(0, len(agencies)-1)
+        return "Congratulations! You managed to look miserable enough so a grandma from %s funding agency gave you some coins!"\
+        %(agencies[curAg])
+
+    @property
+    def failureMessage(self):
+        """
+        :return:  Message which can be displayed in case of failure
+        """
+        curAg = random.randint(0, len(agencies)-1)
+        return "Alas! Your alms box stays empty."
